d2233dc89ce4ec891460229f4d836e545964863b
Fooler <61437388+foole4@users.noreply.github.com>
2026-03-03 18:27:01 +0300

    Update test.py delete

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-#!/usr/bin/env python3
-import time
-import os
-from datetime import datetime
 
-print("🚀 [DOCKER TEST] Сервер запущен!")
-print(f"📅 {datetime.now().strftime('%H:%M:%S')}")
-print(f"📁 {os.getcwd()}")
-print("✅ GitHub → Docker → Python = OK")
-
-# БЕСКОНЕЧНЫЙ ЦИКЛ ДЛЯ 24/7
-counter = 0
-print("🔄 Режим 24/7 активирован...")
-while True:
-    counter += 1
-    print(f"⏰ Uptime: {counter}s | Docker: UP | GitHub: SYNC")
-    time.sleep(2)
